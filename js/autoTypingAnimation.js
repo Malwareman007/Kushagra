48624634f39740820f7cf72f04a2851e80e9e75e
Warmonger <86009160+Malwareman007@users.noreply.github.com>
2023-06-06 14:48:48 +0530

    commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Kushagra Ojha</span> <br> <span>I am a Exploit Developer, Malware Analyst and Security Researcher. I am also WAHS Professional and CEH Trained . I am In top 5 Elite Hacker at hackthebox in India . </span>"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
