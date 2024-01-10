1f33ee87402dd11a3bdd1e931af22d3d3bf5206e
Warmonger <86009160+Malwareman007@users.noreply.github.com>
2024-01-10 08:58:18 +0530

    Commit

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hi, I am Kushagra Ojha</span> <br> <span>I am a Exploit Developer, Malware Analyst and Security Researcher. I am also WAHS Professional and CEH Trained . I am In top 5 Elite Hacker at hackthebox in India . </span>"
+        "<span>Hi, I am Kushagra Ojha</span> <br> <span>I am an Exploit Developer, Malware Analyst, and Security Researcher. I am also a WAHS Professional and CEH. I am In the top 5 Elite Hackers at hackthebox in India. </span>"
       ],
       typeSpeed: 7,
       contentType: 'html'
